5dbcdde25fb966f942ff0a4b8e4763c351169ba2
Lohit <162778623+Lohitkallugol@users.noreply.github.com>
2024-07-10 16:55:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ mongoose.connect(mongoURI)
 
 // Routes
 app.use('/api/auth', authRoutes);
-
+app.get("/",(req,res)=>{
+    res.send("Hello world");
+})
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
